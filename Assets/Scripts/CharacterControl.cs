bbd5c683c11b75ba7587f780877c42b1c5830f93
Midgard Almeida <malmeidamdam1@gmail.com>
2023-11-07 22:16:26 +0100

    Commit iniciando personaje

---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+
+   public float velocidad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocidad = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        +
+
+    }
+}
