f2d0edb8850c35df493e56d863ec2234b8fcfb7a
Midgard <malmeidamdam1@gmail.com>
2023-11-08 14:08:11 +0100

    Pintando mapa

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
-
-   public float velocidad;
+    private Rigidbody2D personaje;
+    public float velocidad;
+    private bool mirandoDrecha;
 
     // Start is called before the first frame update
     void Start()
     {
+        personaje = GetComponent<Rigidbody2D>();
         velocidad = 5;
+        mirandoDrecha = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        +
-
+        ProcesarMovimiento();
     }
+
+    void ProcesarMovimiento() 
+    {
+        //Devuelve negativo si presiona a izquierda/ Positivo si presiona a derecha / 0 si quieto
+        float inputMovimiento = Input.GetAxis("Horizontal");
+
+        personaje.velocity = new Vector2(inputMovimiento * velocidad, personaje.velocity.y);
+
+        GestionarOrientacion(inputMovimiento);
+    }
+
+    void GestionarOrientacion(float inputMovimiento)
+    {
+
+        if(mirandoDrecha==true && inputMovimiento < 0 || mirandoDrecha == false && inputMovimiento > 0 )
+        {
+            mirandoDrecha = !mirandoDrecha;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+    }
+
 }
